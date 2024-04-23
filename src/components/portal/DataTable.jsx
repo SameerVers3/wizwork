6f2116a7f4d066b26ab750a998d04cba4cecc816
alinazir105 <alinazir105@gmail.com>
2024-04-23 16:39:01 +0500

    applications portal

--- src/components/portal/DataTable.jsx
+++ src/components/portal/DataTable.jsx
@@ -2,32 +2,33 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'fullName', headerName: 'Full Name', width: 130 },
-  { field: 'hiringStage', headerName: 'Hiring Stage', width: 130 },
-    // field: 'fullName',
-    // headerName: 'Full name',
-    // description: 'This column has a value getter and is not sortable.',
-    // sortable: false,
-    // width: 160,
-    // valueGetter: (value, row) => `${row.firstName || ''} ${row.lastName || ''}`,
+  { field: 'id', headerName: 'ID', width: 150 },
+  { field: 'fullName', headerName: 'Full Name', width: 250 },
+  { field: 'hiringStage', headerName: 'Hiring Stage', width: 250 },
+  { 
+    field: 'appliedDate', 
+    headerName: 'Applied Date', 
+    width: 250,  
+  },
+  { field: 'jobRole', headerName: 'Job Role', width: 150}
 ];
 
+
 const rows = [
-  { id: 1, fullName: 'Snow', hiringStage: 'Jon', appliedDate: "01/24" },
-  { id: 2, fullName: 'Lannister', hiringStage: 'Cersei', appliedDate: 42 },
-  { id: 3, fullName: 'Lannister', hiringStage: 'Jaime', appliedDate: 45 },
-  { id: 4, fullName: 'Stark', hiringStage: 'Arya', appliedDate: 16 },
-  { id: 5, fullName: 'Targaryen', hiringStage: 'Daenerys', appliedDate: null },
-  { id: 6, fullName:'Melisandre', hiringStage: null, appliedDate: 150 },
-  { id: 7, fullName: 'Clifford', hiringStage: 'Ferrara', appliedDate: 44 },
-  { id: 8, fullName: 'Frances', hiringStage: 'Rossini', appliedDate: 36 },
-  { id: 9, fullName: 'Roxie', hiringStage: 'Harvey', appliedDate: 65 },
+  { id: 1, fullName: 'Snow', hiringStage: 'Jon', appliedDate: '01/24', jobRole: 'Developer' },
+  { id: 2, fullName: 'Lannister', hiringStage: 'Cersei', appliedDate: '02/24', jobRole: 'Developer' },
+  { id: 3, fullName: 'Lannister', hiringStage: 'Jaime', appliedDate: '02/24', jobRole: 'Developer' },
+  { id: 4, fullName: 'Stark', hiringStage: 'Arya', appliedDate: '02/24', jobRole: 'Developer' },
+  { id: 5, fullName: 'Targaryen', hiringStage: 'Daenerys', appliedDate: '02/24', jobRole: 'Developer' },
+  { id: 6, fullName:'Melisandre', hiringStage: 'hello', appliedDate: '02/24', jobRole: 'Developer' },
+  { id: 7, fullName: 'Clifford', hiringStage: 'Ferrara', appliedDate: '02/24', jobRole: 'Developer' },
+  { id: 8, fullName: 'Frances', hiringStage: 'Rossini', appliedDate: '02/24', jobRole: 'Developer' },
+  { id: 9, fullName: 'Roxie', hiringStage: 'Harvey', appliedDate: '02/24', jobRole: 'Developer' },
 ];
 
 export default function DataTable() {
   return (
-    <div style={{ height: 400, width: '100%'}} className='border p-4'>
+    <div style={{ height: 400, width: '98%'}} className='border p-6 ml-4'>
       <DataGrid
         rows={rows}
         columns={columns}
